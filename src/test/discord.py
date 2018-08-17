4011747da806fbba4346befd20ea36b4caa86d3a
Kenny <kalbert9@yahoo.com>
2018-08-16 20:42:37 -0400

    added functions

--- src/test/discord.py
+++ src/test/discord.py
@@ -1,2 +1,20 @@
+class DiscordBot:
+    def __init__(self):
+        pass
 
-print('Hello World')
+    def winner(self):
+        userinput = input('Type !winner if you just won or type !leaders to see to see leaderboard.\n')
+        userinput = userinput.lower()
+        if userinput == '!winner':
+            print('Congrats on Winning!')
+            self.addname()
+        elif userinput == '!leaders':
+           self.leaderboard()
+        else:
+            print ('Not a correct command.')
+            
+    def addname(self):
+        pass
+
+    def leaderboard(self):
+        pass
